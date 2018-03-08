0f303fa432e54fdcf583929623934c2e224ff794
krishnak12344 <khamarkrishna@yahoo.com>
2018-03-07 20:20:08 -0500

    removed extra function

--- client.php
+++ client.php
@@ -70,28 +70,7 @@ function searchInsurance($location,$insurance){
     echo "\n\n";
     echo $argv[0]." END".PHP_EOL;
 }
-function searchSpeciality($location,$speciality){
-    $client = new rabbitMQClient("testRabbitMQ.ini","apiServer");
-    if (isset($argv[1]))
-    {
-      $msg = $argv[1];
-    }
-    else
-    {
-      $msg = "test message";
-    }
-    $request = array();
-    $request['type'] = "speciality";
-    $request['location'] = $location;
-    $request['speciality'] = $speciality;
-    $response = $client->send_request($request);
-    //$response = $client->publish($request);
-    //echo "client received response: ".PHP_EOL;
-    //print_r($response);
-    return $response;
-    echo "\n\n";
-    echo $argv[0]." END".PHP_EOL;
-}
+
 function searchUid($uid){
     $client = new rabbitMQClient("testRabbitMQ.ini","apiServer");
     if (isset($argv[1]))
