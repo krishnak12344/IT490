f5bf00cf5a9a86933e30159947bba37f72d07da8
krishnak12344 <khamarkrishna@yahoo.com>
2018-03-07 19:51:24 -0500

    called uid function

---
+++ uid.php
@@ -0,0 +1,18 @@
+<?php
+
+error_reporting(-1);
+ini_set('display_errors',true);
+
+include('client.php');
+function uid($uid){
+
+
+$response = searchUid($uid);
+
+header('Content-Type: apllication/json;charset=utf-8');
+
+echo($response);
+return $response;
+}
+
+ ?>
