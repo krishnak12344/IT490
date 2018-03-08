922ad095365843cf693e60c20d44aab495144b08
krishnak12344 <khamarkrishna@yahoo.com>
2018-03-07 20:36:37 -0500

    renamed function

--- client.php
+++ client.php
@@ -5,28 +5,7 @@ require_once('path.inc');
 require_once('get_host_info.inc');
 require_once('rabbitMQLib.inc');
 
-function searchLocation($location){
-    $client = new rabbitMQClient("testRabbitMQ.ini","apiServer");
-    if (isset($argv[1]))
-    {
-      $msg = $argv[1];
-    }
-    else
-    {
-      $msg = "test message";
-    }
-    $request = array();
-    $request['type'] = "location";
-    $request['location'] = $location;
-    $response = $client->send_request($request);
-    //$response = $client->publish($request);
-    //echo "client received response: ".PHP_EOL;
-    //print_r($response);
-    return $response;
-    echo "\n\n";
-    echo $argv[0]." END".PHP_EOL;
-}
-function searchSpeciality($location,$speciality){
+function searchSpec($location,$speciality){
     $client = new rabbitMQClient("testRabbitMQ.ini","apiServer");
     if (isset($argv[1]))
     {
@@ -70,7 +49,27 @@ function searchInsurance($location,$insurance){
     echo "\n\n";
     echo $argv[0]." END".PHP_EOL;
 }
-
+function searchLoc($location){
+    $client = new rabbitMQClient("testRabbitMQ.ini","apiServer");
+    if (isset($argv[1]))
+    {
+      $msg = $argv[1];
+    }
+    else
+    {
+      $msg = "test message";
+    }
+    $request = array();
+    $request['type'] = "location";
+    $request['location'] = $location;
+    $response = $client->send_request($request);
+    //$response = $client->publish($request);
+    //echo "client received response: ".PHP_EOL;
+    //print_r($response);
+    return $response;
+    echo "\n\n";
+    echo $argv[0]." END".PHP_EOL;
+}
 function searchUid($uid){
     $client = new rabbitMQClient("testRabbitMQ.ini","apiServer");
     if (isset($argv[1]))
