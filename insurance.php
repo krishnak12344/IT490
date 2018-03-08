bd69eec2fba76c58fb0ed80675667483fef6b53d
krishnak12344 <khamarkrishna@yahoo.com>
2018-03-07 19:49:27 -0500

    called insurance function

---
+++ insurance.php
@@ -0,0 +1,18 @@
+<?php
+
+error_reporting(-1);
+ini_set('display_errors',true);
+
+include('client.php');
+function insurance($location,$insurance){
+
+
+$response = searchInsurance($location,$insurance);
+
+header('Content-Type: apllication/json;charset=utf-8');
+
+echo($response);
+return $response;
+}
+
+ ?>
