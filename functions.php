dc1035eade438ecdfbf68fa0dca3035036163b50
krishnak12344 <35708471+krishnak12344@users.noreply.github.com>
2018-03-12 17:52:37 -0400

    added error logs

--- functions.php
+++ functions.php
@@ -1,5 +1,9 @@
 #!/usr/bin/php
 <?php
+ini_set("display_errors", 0);
+ini_set("log_errors",1);
+ini_set("error_log", "/tmp/error.log");
+error_reporting( E_ALL & ~E_DEPRECATED & ~E_STRICT);
 require_once('path.inc');
 require_once('get_host_info.inc');
 require_once('rabbitMQLib.inc');
